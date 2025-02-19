b41701d1f661fce097180c289ff4444dea81b567
Ardiantohendy <bayuardianto421@gmail.com>
2025-02-19 18:35:00 +0700

    first commit

---
+++ index.js
@@ -0,0 +1,35 @@
+// let username;
+
+// document.getElementById(`myText`).textContent = username;
+
+// let username;
+
+// document.getElementById(`myBtn`).onclick = function () {
+//   username = document.getElementById(`myText`).value;
+//   document.getElementById(`h1`).textContent = `Welcome ${username}`;
+//   console.log(username);
+// };
+
+const myLabel1 = document.getElementById(`myLabel1`);
+const myLabel2 = document.getElementById(`myLabel2`);
+const myLabel3 = document.getElementById(`myLabel3`);
+const rollBtn = document.getElementById("rollBtn");
+const anouncement = document.getElementById("alert");
+
+let randomNum1;
+let randomNum2;
+let randomNum3;
+
+rollBtn.onclick = function () {
+  randomNum1 = Math.floor(Math.random() * 6) + 1;
+  randomNum2 = Math.floor(Math.random() * 6) + 1;
+  randomNum3 = Math.floor(Math.random() * 6) + 1;
+
+  myLabel1.textContent = randomNum1;
+  myLabel2.textContent = randomNum2;
+  myLabel3.textContent = randomNum3;
+
+  if (randomNum1 === randomNum2 && randomNum2 === randomNum3) {
+    anouncement.style.opacity = 100;
+  }
+};
